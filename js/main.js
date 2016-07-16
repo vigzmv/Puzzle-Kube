1ef8ec6ef80a7f2e1809a7156812f511579746b4
Vignesh <vigzmv@outlook.com>
2016-07-16 09:41:25 +0530

    +1 -> htadg

--- js/main.js
+++ js/main.js
@@ -1,29 +1,56 @@
+var boxes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+var factorial = function (number){
+	if (number ===0 || number === 1)
+		return 1;
+	return factorial(number-1)*number;
+};
+
+var index = 0;
+
 var bigbox = document.getElementById("bigbox");
 var boxH = getComputedStyle(bigbox).getPropertyValue("height");
-var boxW = getComputedStyle(bigbox).getPropertyValue("width");
-boxH = boxH.slice(0,boxH.length - 2);
-boxW = boxW.slice(0,boxW.length - 2);
 
-function createSmallBox(noOfBoxes){
+var max_size = factorial(Math.max(...boxes));
+var bigBoxSize = boxH.slice(0,boxH.length - 2);
+
+function createSmallBox(total_extra_space, no_of_box){
 	
-	var h = boxH - ((noOfBoxes - 1) * 5) - Math.round(0.005*boxH);
-	var w = boxW - ((noOfBoxes - 1) * 5) - Math.round(0.005*boxW);
+	var h = bigBoxSize - total_extra_space;
 
-	h = h/noOfBoxes + "px"
-	w = w/noOfBoxes + "px"
+	console.log(h);
 
-	for (var i = 0; i < (noOfBoxes*noOfBoxes); i++) {
+	h = h/no_of_box + "px";
+
 	var SmallBox = document.createElement("div");
 
 	SmallBox.id = "SmallBox"
 	SmallBox.style.height = h;
-	SmallBox.style.width = w;
-	SmallBox.style.backgroundColor = "red";
-	SmallBox.style.float = "left";
-	SmallBox.style.borderRadius = "5px"
-	SmallBox.style.margin = "2px"
-
-	
+	SmallBox.style.width = h;	
 	bigbox.appendChild(SmallBox);
+}
+
+function create(){
+	for(var i=0; i<boxes[index]*boxes[index]; i++){
+		var extra_spaces = 6 * boxes[index];
+		createSmallBox(extra_spaces, boxes[index]);
 	}
 }
+
+function start(){
+	if(index >= boxes.length)
+		window.alert("You have reached the end of the road Pal!");
+	else{
+		var bigBox = document.getElementById('bigbox');
+		while(bigBox.firstChild){
+			bigBox.removeChild(bigBox.firstChild);
+		}
+		create();
+		index++;
+	}
+}
+
+window.onload = function () {
+	start();
+	alert("Click as many times as you want!");
+};
